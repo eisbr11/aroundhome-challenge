ab437a1e4ad0f63fe414e70a240c2fbac6464d04
Felix Schliemann <felix.schliemann@diva-e.com>
2021-02-14 22:04:20 +0100

    feat: changed testing action file name

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders coding challenge footer text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Coding Challenge by Felix Schliemann/i);
-  expect(linkElement).toBeInTheDocument();
+  const footerElement = screen.getByText(/Coding Challenge by Felix Schliemann/i);
+  expect(footerElement).toBeInTheDocument();
 });
